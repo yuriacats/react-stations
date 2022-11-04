a0acf6cfe19013035ce386f58f2aa92c3720c580
HimekaTsuchida <yuria.soft0615@gmail.com>
2022-11-04 11:13:53 +0000

    犬を表示するアプリ

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ export const App = () => {
     <div>
       <header>
         <h1>yuriacats's AppLinks</h1>
+        <p>犬の画像が今は表示されています。</p>
+        <img src="https://images.dog.ceo/breeds/dingo/n02115641_4677.jpg" />
       </header>
     </div>
   )
