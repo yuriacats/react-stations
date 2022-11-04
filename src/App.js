9af9cb8f206ae3efd823cf9b5b94d80d48cf0ac5
HimekaTsuchida <yuria.soft0615@gmail.com>
2022-11-04 11:22:25 +0000

    犬を二種類表示

--- src/App.js
+++ src/App.js
@@ -9,10 +9,14 @@ import './App.css'
  */
 export const App = () => {
   const [dog, setDog] = useState("https://images.dog.ceo/breeds/dingo/n02115641_4677.jpg")
+  const changeDog = () => {
+    setDog("https://images.dog.ceo/breeds/shiba/shiba-19.jpg")
+  }
   return (
     <div>
       <header>
         <h1>yuriacats's AppLinks</h1>
+        <button onClick={changeDog} >更新！！</button>
         <p>犬の画像が今は表示されています。</p>
         <img src={dog} />
       </header>
