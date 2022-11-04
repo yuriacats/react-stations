936f1895095b28b5147e5154062137eb47f9808a
HimekaTsuchida <yuria.soft0615@gmail.com>
2022-11-04 11:08:05 +0000

    アプリ名、決まらない

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ import './App.css'
 export const App = () => {
   return (
     <div>
-      <h2>Hello, world!</h2>
+      <header>
+        <h1>yuriacats's AppLinks</h1>
+      </header>
     </div>
   )
 }
